018d1f72f0f0ac6fbb5cabf1730ce2e48b7310c0
Hiroyuki Sato <hiroyuki_sato@spiber.jp>
2014-11-21 12:52:46 +0900

    Enable to find over 100 records
    fixes #1

--- lib/cyby/kintone/app.rb
+++ lib/cyby/kintone/app.rb
@@ -1,12 +1,31 @@
 module Cyby
   module Kintone
     class App
+      LIMIT = 100
+
       def initialize(id)
         @api = RestApi.new(id)
       end
 
       def find(params)
-        resp = @api.get('/records.json', params)
+        result = []
+        page = 0
+        begin
+          records = find_per_page(params, page)
+          result.concat(records)
+          page += 1
+        end while records.count == LIMIT
+        result
+      end
+
+      def find_per_page(params, page)
+        params_per_page = params.dup
+        queries = [params[:query]]
+        if page > 0
+          queries << "offset #{LIMIT * page}"
+        end
+        params_per_page[:query] = queries.join(" ")
+        resp = @api.get('/records.json', params_per_page)
         resp['records'].map do |record|
           Record.new(record)
         end
--- lib/cyby/version.rb
+++ lib/cyby/version.rb
@@ -1,3 +1,3 @@
 module Cyby
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
